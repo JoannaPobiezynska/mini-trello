8a07f9d843b3560f2892290cebf25eaff3eb7052
joannapobiezynska <joanna@chainfire.co.uk>
2019-06-16 14:47:12 +0100

    create basic single Task component, import styles

---
+++ src/components/TaskList/Task/Task.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import classes from './Task.module.css';
+
+class Task extends Component {
+    render() {
+        return <h1>Task component</h1>
+    }
+}
+
+export default Task;
